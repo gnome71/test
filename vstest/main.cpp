f95e6ddc58954ee9fb3af27749559e46f6362c3d
Alexander Kaspar <alexander.kaspar@gmail.com>
2017-01-04 15:41:40 +0100

    no generate error

--- vstest/main.cpp
+++ vstest/main.cpp
@@ -1,14 +1,8 @@
-#include <QTextStream>
+#include <QDebug>
 
-int main(int argc, char **argv) {
-	QVector<int> a; // Qt object
-
-	for (int i = 0; i<10; i++) {
-		a.append(i);
-	}
-
-	for (int i : a)
-		QTextStream(stdout) << "Counter: " << i;
-
+int main(int argc, char *argv[])
+{
+	qDebug() << "hello world!";
 	return 0;
 }
+
